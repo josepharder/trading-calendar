0d5d74803cb13c933f088ff6a118f3d6d33d5e39
Jose <joseharder13t@gmail.com>
2025-01-03 20:04:36 -0600

    Initialize trading calendar repo

---
+++ src/components/BaseTemplate.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+function increase(): void {
+  counter.value++;
+}
+</script>
+
+<template>
+  <div>Hello {{ counter }}</div>
+  <button @click="increase()">Increase</button>
+</template>
