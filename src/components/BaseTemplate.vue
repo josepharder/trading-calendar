05ac28441fdafc605da151fdebff6be843345a25
Jose <joseharder13t@gmail.com>
2025-12-29 11:37:34 -0600

    Improve calendar styling

--- src/components/BaseTemplate.vue
+++ src/components/BaseTemplate.vue
@@ -6,10 +6,11 @@
 
 <style lang="less">
 body {
-  background: #f7f7f7;
+  background: #1a1f2a;
   height: 100vh;
   width: 100vw;
   margin: 0;
   padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif;
 }
 </style>
